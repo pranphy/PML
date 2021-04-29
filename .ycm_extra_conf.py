98d190fe599086d2bf213fc5f6d98c4ccac70f78
Prakash Gautam <pranphy@gmail.com>
2021-04-29 17:02:42 -0400

    Add ycm config file

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+import os
+
+def get_std_flags():
+    flags = [
+    '-Wall',
+    '-Wextra',
+    '-Wc++11-compat',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-x', 'c++',
+    '-fexceptions',
+    '-DNDEBUG',
+    '-std=c++17',
+    '-I','include',
+    '-I','~/MyRoot/usr/include',
+    ]
+    return flags
+
+def get_config_params(config):
+    got  = subprocess.run(config.split(),stdout=subprocess.PIPE)
+    rvalue = got.stdout.decode('utf-8').strip()
+    made = rvalue.replace('-I','-I ').split()
+    return made 
+
+def Settings(**kwargs):
+    config ="wx-config --cflags"
+    flags = get_std_flags() + get_config_params(config)
+    rmap =  {
+        'flags': flags,
+        'do_cache': True
+    }
+    return rmap;
+
+if __name__ == '__main__':
+    print(Settings())
+
